ccf02d0c0e4e9b87d7a1d4f30abeb39ee90aa787
Alex Cameron <72214882+alexcameron8@users.noreply.github.com>
2024-01-24 16:58:19 -0500

    Improved UI design. Added Timer logic.

--- src/App.jsx
+++ src/App.jsx
@@ -68,6 +68,7 @@ function App() {
   return (
     <div className="app">
       <Header />
+      <DriverProfiles />
       <Footer />
     </div>
   );
@@ -101,15 +102,16 @@ function Header() {
     fetchData();
   }, []);
   return (
-    <div className="header">
+    <div className="header-container">
       {loading ? (
         <div className="race-loading"></div>
       ) : (
-        <div>
-          <SelectDropdown raceData={raceData} onSelect={handleSelectRaceId} />
-          <RaceInformation selectedRace={raceData[selectedRaceId]} />
+        <div className="header">
+          <div className="race-info-container">
+            <SelectDropdown raceData={raceData} onSelect={handleSelectRaceId} />
+            <RaceInformation selectedRace={raceData[selectedRaceId]} />
+          </div>
           <RaceTimer selectedRace={raceData[selectedRaceId]} />
-          <DriverProfiles />
         </div>
       )}
     </div>
@@ -117,29 +119,201 @@ function Header() {
 }
 
 const SelectDropdown = ({ raceData, onSelect }) => {
+  const [raceList, setRaceList] = useState(raceData);
+
+  function handleSetRaceList(type) {
+    const orderedRaceDate = raceData
+      .slice()
+      .sort((a, b) => a.raceName.localeCompare(b.raceName));
+
+    if (type === "numeric") {
+      console.log("Num: rounds in order");
+      console.log(raceData);
+      setRaceList(raceData);
+    } else {
+      console.log("Alpha: rounds random order");
+      console.log(orderedRaceDate);
+      setRaceList(() => orderedRaceDate);
+    }
+    // console.log(raceList);
+  }
+
   return (
-    <select
-      className="race-select"
-      onChange={(e) => onSelect(e.target.selectedIndex)}
-    >
-      {raceData.map((race) => (
-        <option key={race["round"]}> {race["raceName"]}</option>
-      ))}
-    </select>
+    <div className="race-line-container">
+      <div className="text-race-info">Select Race:</div>
+      <select
+        required
+        className="race-select"
+        onChange={(e) => onSelect(e.target.selectedIndex)}
+      >
+        {raceList.map((race) => (
+          <option className="race-option" key={race["round"]}>
+            {race["raceName"]}
+          </option>
+        ))}
+      </select>
+      <button className="btn-sort" onClick={() => handleSetRaceList("numeric")}>
+        <img
+          className="img-btn-sort"
+          src="/src/assets/sort-by-numeric-order.png"
+          alt="sort-numerical"
+        />
+      </button>
+      <button
+        className="btn-sort"
+        onClick={() => handleSetRaceList("alphabetical")}
+      >
+        <img
+          className="img-btn-sort"
+          src="/src/assets/alphabetical-order.png"
+          alt="sort-alphabetical"
+        />
+      </button>
+    </div>
   );
 };
 
 function RaceInformation({ selectedRace }) {
+  function getEmojiFlag(country) {
+    switch (country) {
+      case "Abu Dhabi Grand Prix":
+        return "🇦🇪";
+      case "Australian Grand Prix":
+        return "🇦🇺";
+      case "Austrian Grand Prix":
+        return "🇦🇹";
+      case "Azerbaijan Grand Prix":
+        return "🇦🇿";
+      case "Bahrain Grand Prix":
+        return "🇧🇭";
+      case "Belgian Grand Prix":
+        return "🇧🇪";
+      case "British Grand Prix":
+        return "🇬🇧";
+      case "Canadian Grand Prix":
+        return "🇨🇦";
+      case "Chinese Grand Prix":
+        return "🇨🇳";
+      case "Dutch Grand Prix":
+        return "🇳🇱";
+      case "Emilia Romagna Grand Prix":
+        return "🇮🇹";
+      case "Hungarian Grand Prix":
+        return "🇭🇺";
+      case "Italian Grand Prix":
+        return "🇮🇹";
+      case "Japanese Grand Prix":
+        return "🇯🇵";
+      case "Las Vegas Grand Prix":
+        return "🇺🇸";
+      case "Mexico City Grand Prix":
+        return "🇲🇽";
+      case "Miami Grand Prix":
+        return "🇺🇸";
+      case "Monaco Grand Prix":
+        return "🇲🇨";
+      case "Qatar Grand Prix":
+        return "🇶🇦";
+      case "São Paulo Grand Prix":
+        return "🇧🇷";
+      case "Saudi Arabian Grand Prix":
+        return "🇸🇦";
+      case "Singapore Grand Prix":
+        return "🇸🇬";
+      case "Spanish Grand Prix":
+        return "🇪🇸";
+      case "United States Grand Prix":
+        return "🇺🇸";
+      default:
+        return "Unknown Flag";
+    }
+  }
+
+  function getCircuitPNG(country) {
+    switch (country) {
+      case "Abu Dhabi Grand Prix":
+        return "/src/assets/circuits/abudhabi.avif";
+      case "Australian Grand Prix":
+        return "/src/assets/circuits/australia.avif";
+      case "Austrian Grand Prix":
+        return "/src/assets/circuits/austria.avif";
+      case "Azerbaijan Grand Prix":
+        return "/src/assets/circuits/azerbaijan.avif";
+      case "Bahrain Grand Prix":
+        return "/src/assets/circuits/bahrain.avif";
+      case "Belgian Grand Prix":
+        return "/src/assets/circuits/belgium.avif";
+      case "British Grand Prix":
+        return "/src/assets/circuits/great-britain.avif";
+      case "Canadian Grand Prix":
+        return "/src/assets/circuits/canada.avif";
+      case "Chinese Grand Prix":
+        return "/src/assets/circuits/china.avif";
+      case "Dutch Grand Prix":
+        return "/src/assets/circuits/netherlands.png";
+      case "Emilia Romagna Grand Prix":
+        return "/src/assets/circuits/italy_imola.png";
+      case "Hungarian Grand Prix":
+        return "/src/assets/circuits/hungary.avif";
+      case "Italian Grand Prix":
+        return "/src/assets/circuits/monza.avif";
+      case "Japanese Grand Prix":
+        return "/src/assets/circuits/japan.avif";
+      case "Las Vegas Grand Prix":
+        return "/src/assets/circuits/las_vegas.avif";
+      case "Mexico City Grand Prix":
+        return "/src/assets/circuits/mexico.avif";
+      case "Miami Grand Prix":
+        return "/src/assets/circuits/miami.avif";
+      case "Monaco Grand Prix":
+        return "/src/assets/circuits/monaco.png";
+      case "Qatar Grand Prix":
+        return "/src/assets/circuits/qatar.avif";
+      case "São Paulo Grand Prix":
+        return "/src/assets/circuits/brazil.avif";
+      case "Saudi Arabian Grand Prix":
+        return "/src/assets/circuits/Saudi_Arabia.png";
+      case "Singapore Grand Prix":
+        return "/src/assets/circuits/singapore.avif";
+      case "Spanish Grand Prix":
+        return "/src/assets/circuits/spain.avif";
+      case "United States Grand Prix":
+        return "/src/assets/circuits/austin.avif";
+      default:
+        return "Unknown Flag";
+    }
+  }
+
   return (
-    <div>
-      <p>
-        Race Location:
-        {selectedRace["Circuit"]["Location"]["country"] +
-          "," +
-          selectedRace["Circuit"]["Location"]["locality"]}
-      </p>
-      <p>Track Name: {selectedRace["Circuit"]["circuitName"]}</p>
-      <p>Date: {selectedRace["date"]}</p>
+    <div className="race-information">
+      <div className="race-line-container">
+        <p> Location: </p>
+        <h1>
+          {selectedRace["Circuit"]["Location"]["country"] +
+            ", " +
+            selectedRace["Circuit"]["Location"]["locality"]}
+        </h1>
+        <div className="country-icon">
+          {getEmojiFlag(selectedRace["raceName"])}
+        </div>
+      </div>
+      <div className="race-line-container">
+        <p> Circuit: </p>
+        <h3>{selectedRace["Circuit"]["circuitName"]}</h3>
+      </div>
+      <div className="race-line-container">
+        <p> Date: </p>
+        <h4>{selectedRace["date"]}</h4>
+      </div>
+      <div className="race-line-container">
+        <p> Race: #{selectedRace["round"]}/24</p>
+      </div>
+      <div className="circuit-container">
+        <img
+          src={getCircuitPNG(selectedRace["raceName"])}
+          alt={selectedRace["round"]}
+        />
+      </div>
     </div>
   );
 }
@@ -218,9 +392,15 @@ function TimerCircle({ type, time, color }) {
     interval_time = time * 60 * 60 * 24;
     total_duration = total_hours;
   }
+
+  // Determine if width is mobile
+  const isMobile = window.innerWidth < 768 || window.visualViewport.width < 768;
+  // Dynamically adjust the size based on screen resolution
+  const timer_size = isMobile ? 100 : 225;
+
   const timerProps = {
     isPlaying: true,
-    size: 225,
+    size: timer_size,
     strokeWidth: 16,
     trailStrokeWidth: 14,
     trailColor: "grey",
@@ -230,17 +410,15 @@ function TimerCircle({ type, time, color }) {
     return (
       <div className="time-wrapper">
         <div className="time">{time}</div>
-        <div>{dimension}</div>
+        <div className="units">{dimension}</div>
       </div>
     );
   };
 
-  console.log(type);
-  console.log(isLoading);
-
   return (
     <div id="countdown">
       {isLoading && (
+        // https://www.npmjs.com/package/react-countdown-circle-timer?activeTab=readme
         <CountdownCircleTimer
           className={`timer-wheel ${type}`}
           {...timerProps}
